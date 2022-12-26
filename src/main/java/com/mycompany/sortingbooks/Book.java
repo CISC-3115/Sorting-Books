787ed62b687ad8fe27040dd16fd448e964c0a45b
naymo <naymo@DESKTOP-1DVHCMP.myfiosgateway.com>
2022-12-26 05:28:07 -0500

    Shortened the main

--- src/main/java/com/mycompany/sortingbooks/Book.java
+++ src/main/java/com/mycompany/sortingbooks/Book.java
@@ -1,10 +1,17 @@
 package com.mycompany.sortingbooks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
 class Book {
 
     String title;
     String author;
     double rating;
+    ArrayList<Book> bookList = new ArrayList<>();
 
     public Book(String title, String author, double rating) {
         this.title = title;
@@ -13,11 +20,51 @@ class Book {
 
     }
 
+    public Book() {
+        bookList = new ArrayList<>();
+
+    }
+
+    public void sort(Comparator x) {
+        bookList.sort(x);
+
+        for (Book book : bookList) {
+            System.out.println(book);
+        }
+    }
+
+    public void populate(String name) throws FileNotFoundException {
+
+        Scanner sc = new Scanner(new File(name));
+        while (sc.hasNext()) {
+
+            try {
+                String x = sc.nextLine();
+                String[] split = x.split(",");
+
+                String names[] = split[1].split(" ");
+
+                if (split.length != 3 || names.length != 2) {
+
+                    throw new Exception();
+                } else {
+
+                    bookList.add(new Book(split[0], split[1], Double.parseDouble(split[2])));
+
+                }
+
+            } catch (Exception e) {
+
+            }
+        }
+
+    }
+
     public String getTitle() {
 
         return title;
     }
-//This method returns the last index after " " hence the last name
+
 
     public String getAuthor() {
 
--- src/main/java/com/mycompany/sortingbooks/SortingBooks.java
+++ src/main/java/com/mycompany/sortingbooks/SortingBooks.java
@@ -2,7 +2,6 @@ package com.mycompany.sortingbooks;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 
@@ -11,10 +10,12 @@ public class SortingBooks {
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner sr = new Scanner(System.in);
-        ArrayList<Book> main = new ArrayList<>();
+
         String name = checkExists(sr);
-        populate(name, main);
-        choices(main, sr);
+        Book x = new Book();
+
+        x.populate(name);
+        choices(x, sr);
     }
 
 // This method checks to see if the name of a file exists
@@ -35,37 +36,9 @@ public class SortingBooks {
         return check;
 
     }
-//This method populates the arraylist
 
-    public static void populate(String name, ArrayList<Book> books) throws FileNotFoundException {
-
-        Scanner sc = new Scanner(new File(name));
-        while (sc.hasNext()) {
-
-            try {
-                String x = sc.nextLine();
-                String[] split = x.split(",");
-
-                String names[] = split[1].split(" ");
-
-                if (split.length != 3 || names.length != 2) {
-
-                    throw new Exception();
-                } else {
-
-                    books.add(new Book(split[0], split[1], Double.parseDouble(split[2])));
-
-                }
-
-            } catch (Exception e) {
-
-            }
-        }
-
-    }
-//This method lists the user choices on how they want to sort the arraylist
-
-    public static void choices(ArrayList<Book> books, Scanner sr) {
+//This method lists the user choices on how they want to sort the object
+    public static void choices(Book books, Scanner sr) {
         boolean flag = true;
         while (flag == true) {
 
@@ -77,16 +50,16 @@ public class SortingBooks {
             switch (c) {
                 case 't':
 
-                   sort(books,Comparator.comparing(Book::getTitle));
+                    books.sort(Comparator.comparing(Book::getTitle));
                     break;
 
                 case 'n':
 
-                   sort(books,Comparator.comparing(Book::getAuthor));
+                    books.sort(Comparator.comparing(Book::getAuthor));
                     break;
                 case 'r':
 
-                    sort(books,Comparator.comparing(Book::getRating));
+                    books.sort(Comparator.comparing(Book::getRating));
                     break;
 
                 case 'q':
@@ -97,19 +70,5 @@ public class SortingBooks {
 
             }
         }
-
     }
-//This method sorts the arraylist based on title alphabetical order
-
-    public static void sort(ArrayList<Book> books, Comparator x) {
-        books.sort(x);
-
-        for (Book book : books) {
-            System.out.println(book);
-        }
-    }
-//This method sorts the arraylist based on last name alphabetical order
-
-   
-
 }
