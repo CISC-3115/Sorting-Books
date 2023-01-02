c72a6fa3e2a65831d2555cbd99d5ceb1dcd14ae2
naymo <naymo@DESKTOP-1DVHCMP.myfiosgateway.com>
2023-01-02 18:43:08 -0500

    Shortened main further by adding a new class

--- src/main/java/com/mycompany/sortingbooks/SortingBooks.java
+++ src/main/java/com/mycompany/sortingbooks/SortingBooks.java
@@ -1,6 +1,5 @@
 package com.mycompany.sortingbooks;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Comparator;
 import java.util.Scanner;
@@ -8,37 +7,17 @@ import java.util.Scanner;
 public class SortingBooks {
 
     public static void main(String[] args) throws FileNotFoundException {
-
         Scanner sr = new Scanner(System.in);
-
-        String name = checkExists(sr);
-        Book x = new Book();
-
+        String name = Book.checkExists(sr);
+        BookList x = new BookList();
         x.populate(name);
         choices(x, sr);
-    }
-
-// This method checks to see if the name of a file exists
-    public static String checkExists(Scanner sr) {
-
-        System.out.print("Books file: ");
-        String check = sr.next();
-        File r = new File(check);
-
-        while (!r.exists()) {
-            System.out.println("File does not exist; try again");
-            System.out.print("Books file: ");
-            check = sr.next();
-            r = new File(check);
-
-        }
-
-        return check;
-
+        
+        
     }
 
 //This method lists the user choices on how they want to sort the object
-    public static void choices(Book books, Scanner sr) {
+    public static void choices(BookList books, Scanner sr) {
         boolean flag = true;
         while (flag == true) {
 
